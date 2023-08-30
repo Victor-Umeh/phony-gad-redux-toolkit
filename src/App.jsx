da2a509951784d901739bc86bc5a65f872dc6e32
vk196 <victorumeh196@gmail.com>
2023-08-30 20:56:47 +0100

    data fetching from local server

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import NavBar from "./components/navBar";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const { isOpen } = useSelector((store) => store.modal);
-  const { cartItems, total } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
-  // console.log(total);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="loading">
+        <h1>Loading</h1>
+      </div>
+    );
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../libs/cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+const url = "https://course-api.com/react-useReducer-cart-project";
 const initialState = {
-  cartItems: cartItems,
-  amount: 0,
+  cartItems: [],
+  amount: 4,
   total: 0,
   isLoading: true,
 };
+
+//fetching data in a redux
+export const getCartItems = createAsyncThunk("cart/getCartIems", () => {
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+});
 
 const cartSlice = createSlice({
   name: "cart",
@@ -40,13 +47,28 @@ const cartSlice = createSlice({
       let amount = 0;
       let total = 0;
 
-      state.cartItems.forEach((item) => {
+      state.cartItems?.forEach((item) => {
         amount += item.amount;
         total += amount * item.price;
       });
 
       state.amount = amount;
       state.total = total;
+    },
+  },
+
+  //asychronous operation actions
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      // console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
   },
 });
